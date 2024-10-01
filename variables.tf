32dd101c1a6a9383c9969b86f4294f48aa28ebb2
Jon Snow <0077amaterasu16@gmail.com>
2024-10-01 19:04:02 -0400

    Primer commit

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "resource_group_location" {
+  type        = string
+  default     = "eastus2"
+  description = "Location of the resource group."
+}
+
+variable "resource_group_name_prefix" {
+  type        = string
+  default     = "app"
+  description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
+}
+
+variable "username" {
+  type        = string
+  description = "The username for the local account that will be created on the new VM."
+  default     = "app"
+}
+
+variable "hostname" {
+  type        = string
+  description = "Nombre que va tener nuestro host"
+  default     = "DDEApp"
+}
