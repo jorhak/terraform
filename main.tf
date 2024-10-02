f56f793dcd1b1e78cb43a8db9a923b59c4a5e1ca
Jon Snow <0077amaterasu16@gmail.com>
2024-10-02 00:50:20 -0400

    Terraform add Prd

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 # Create virtual network
-resource "azurerm_virtual_network" "my_terraform_network" {
+resource "azurerm_virtual_network" "my_terraform_networkPrd" {
   name                = "myVnetPrd"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
@@ -16,7 +16,7 @@ resource "azurerm_virtual_network" "my_terraform_network" {
 }
 
 # Create subnet
-resource "azurerm_subnet" "my_terraform_subnet" {
+resource "azurerm_subnet" "my_terraform_subnetPrd" {
   name                 = "mySubnetPrd"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.my_terraform_network.name
@@ -24,7 +24,7 @@ resource "azurerm_subnet" "my_terraform_subnet" {
 }
 
 # Create public IPs
-resource "azurerm_public_ip" "my_terraform_public_ip" {
+resource "azurerm_public_ip" "my_terraform_public_ipPrd" {
   name                = "myPublicIPPrd"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
@@ -32,8 +32,8 @@ resource "azurerm_public_ip" "my_terraform_public_ip" {
 }
 
 # Create Network Security Group and rule
-resource "azurerm_network_security_group" "my_terraform_nsg" {
-  name                = "myNetworkSecurityGroup"
+resource "azurerm_network_security_group" "my_terraform_nsgPrd" {
+  name                = "myNetworkSecurityGroupPrd"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
@@ -73,7 +73,7 @@ resource "azurerm_network_security_group" "my_terraform_nsg" {
 }
 
 # Create network interface
-resource "azurerm_network_interface" "my_terraform_nic" {
+resource "azurerm_network_interface" "my_terraform_nicPrd" {
   name                = "myNICPrd"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
@@ -103,7 +103,7 @@ resource "random_id" "random_id" {
 }
 
 # Create storage account for boot diagnostics
-resource "azurerm_storage_account" "my_storage_account" {
+resource "azurerm_storage_account" "my_storage_accountPrd" {
   name                     = "diag${random_id.random_id.hex}"
   location                 = azurerm_resource_group.rg.location
   resource_group_name      = azurerm_resource_group.rg.name
@@ -112,7 +112,7 @@ resource "azurerm_storage_account" "my_storage_account" {
 }
 
 # Create virtual machine
-resource "azurerm_linux_virtual_machine" "my_terraform_vm" {
+resource "azurerm_linux_virtual_machine" "my_terraform_vmPrd" {
   name                  = "myPrd"
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
