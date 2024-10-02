a1a5a53a3e4016e04f997db4db24275e9d4a7a66
Jon Snow <0077amaterasu16@gmail.com>
2024-10-02 00:58:00 -0400

    Ansible hosts, credentials and script install docker

--- script.sh
+++ script.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+
 # Add Docker's official GPG key:
-sudo apt-get update -y
-sudo apt-get install ca-certificates curl -y
-sudo install -m 0755 -d /etc/apt/keyrings -y
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
@@ -11,12 +12,19 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update -y
+sudo apt-get update
 
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# Install last version
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Verify install
+sudo docker run hello-world
+
+# Create group docker
 sudo groupadd docker
+
+# Add USER a group docker
 sudo usermod -aG docker $USER
 newgrp docker
-
-sudo systemctl enable docker.service
-sudo systemctl enable containerd.service
+# Run sin SUDO
+docker run hello-world
